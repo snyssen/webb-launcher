17ef848000b8fdb69a588ab8ae37be42afe52fd6
Simon Nyssen <simon.nyssen@mailfence.com>
2022-09-22 18:16:03 +0200

    Enables SSR with node.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server',
+  integrations: [tailwind()],
+  adapter: node()
 });

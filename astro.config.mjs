243955d668054e0a3c28e775eec3f361a4443d64
Simon Nyssen <simon.nyssen@mailfence.com>
2022-09-22 17:38:44 +0200

    Enables SSR with Deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server',
+  integrations: [tailwind()],
+  adapter: deno()
 });

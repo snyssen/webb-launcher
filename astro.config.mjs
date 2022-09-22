62e2af2fbcd24390bfa54b83f916f16164f96869
Simon Nyssen <simon.nyssen@mailfence.com>
2022-09-22 17:38:44 +0200

    Revert "Enables SSR with Deno"
    This reverts commit 243955d668054e0a3c28e775eec3f361a4443d64.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
-import deno from "@astrojs/deno";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [tailwind()],
-  adapter: deno()
+  integrations: [tailwind()]
 });

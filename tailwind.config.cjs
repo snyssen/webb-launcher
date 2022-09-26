56e079d0faa2bb7aec61913799c6652db2262743
Simon Nyssen <simon.nyssen@mailfence.com>
2022-09-26 15:51:15 +0200

    Configures Preline in Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}',
+		'node_modules/preline/dist/*.js',
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('preline/plugin')
+	],
 }

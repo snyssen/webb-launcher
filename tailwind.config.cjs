d2bce48a33e75ddb8c2cefe46418452247d947f3
Simon Nyssen <simon.nyssen@mailfence.com>
2022-09-27 12:14:18 +0200

    Removes Preline and adds dark theme switch

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}',
-		'node_modules/preline/dist/*.js',
 	],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		require('preline/plugin')
 	],
 }

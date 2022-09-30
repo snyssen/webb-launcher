cc81cd968c40e60c0b95be1d0f045f2b2f26897b
Simon Nyssen <simon.nyssen@mailfence.com>
2022-09-30 17:42:07 +0200

    Adds Octarine Light font as default font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'sans': ['Octarine Light', ...defaultTheme.fontFamily.sans]
+			}
+		},
 	},
 	plugins: [
 	],

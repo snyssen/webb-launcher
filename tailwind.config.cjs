d12403f311b59f190a879a91b94ede05833d1e72
Simon Nyssen <simon.nyssen@mailfence.com>
2022-10-04 09:33:42 +0200

    Adds gradient animation on all shortcuts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,23 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				'sans': ['Octarine Light', ...defaultTheme.fontFamily.sans]
+			},
+			backgroundSize: {
+				'150%': '150%',
+				'200%': '200%',
+				'250%': '250%',
+				'300%': '300%',
+				'350%': '350%',
+				'400%': '400%',
+			},
+			keyframes: {
+				'gradient-slide-left-right': {
+					'0%': { 'background-position': '0% 50%' },
+					'100%': { 'background-position': '100% 50%' }
+				}
+			},
+			animation: {
+				'gradient-pulse-once': 'gradient-slide-left-right 0.5s ease-out forwards'
 			}
 		},
 	},

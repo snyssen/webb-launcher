b0aab558b693c5f31986e189ad737f47d54ffef0
Simon Nyssen <dev@snyssen.be>
2022-10-28 09:50:04 +0200

    Fixes speed unit in metrics

--- src/configuration/OpenWeatherConfig.ts
+++ src/configuration/OpenWeatherConfig.ts
@@ -24,7 +24,7 @@ export abstract class OpenWeatherConfig {
   static getDisplayableSpeedUnit = () => {
     switch (OpenWeatherConfig.units) {
       case 'metric':
-        return 'km/h';
+        return 'm/s';
       case 'imperial':
         return 'mph';
       case 'standard':

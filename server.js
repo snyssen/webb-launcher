90db578b02b493a1d24bfeb46eccf98c3979ba25
Simon Nyssen <dev@snyssen.be>
2024-02-10 14:27:30 +0100

    build: add updated build & release pipeline

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-import express from 'express';
-import { handler as ssrHandler } from './dist/server/entry.mjs';
+import express from "express";
+import { handler as ssrHandler } from "./dist/server/entry.mjs";
 
 const app = express();
-app.use(express.static('dist/client/'))
+app.use(express.static("dist/client/"));
 app.use(ssrHandler);
 
 const port = 8080;
 app.listen(port);
 
-console.log('Webb launcher version', process.env.LAUNCHER_VERSION ?? 'unkown', 'listening on port:', port);
+console.log(
+  "Webb launcher version",
+  process.env.APP_VERSION ?? "unkown",
+  "listening on port:",
+  port
+);
